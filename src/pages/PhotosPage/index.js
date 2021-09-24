a5d9f127761880a12e1083c7edcc185cbe780393
ManuelBouzaCachaza <84750625+ManuelBouzaCachaza@users.noreply.github.com>
2021-09-24 13:56:58 +0200

    cambios

--- src/pages/PhotosPage/index.js
+++ src/pages/PhotosPage/index.js
@@ -3,33 +3,39 @@ import usePhotos from "../../hooks/usePhotos";
 import Photo from "../../components/Photo";
 import List from "../../components/List";
 import { useParams } from "react-router";
+import SearchForm from "../../components/SearchForm";
 
 const PhotosPage = () => {
   const { id } = useParams();
   const [photos] = usePhotos(id);
 
   return (
-    <div className="lista-photos">
-      {photos.length > 0 && (
-        <List
-          data={photos}
-          render={(photo) => (
-            <Photo
-              key={photo.id_photo}
-              created_at={photo.created_at}
-              id_photo={photo.id_photos}
-              url={photo.url}
-              id_user={photo.id_users}
-              likes={photo.likes}
-              numComentarios={photo.numComentarios}
-              place={photo.place}
-              userName={photo.username}
-              userAvatar={photo.userphoto}
-            />
-          )}
-        />
-      )}
-    </div>
+    <>
+      {/* <div>
+        <SearchForm />
+      </div> */}
+      <div className="lista-photos">
+        {photos.length > 0 && (
+          <List
+            data={photos}
+            render={(photo) => (
+              <Photo
+                key={photo.id_photo}
+                created_at={photo.created_at}
+                id_photo={photo.id_photos}
+                url={photo.url}
+                id_user={photo.id_users}
+                likes={photo.likes}
+                numComentarios={photo.numComentarios}
+                place={photo.place}
+                userName={photo.username}
+                userAvatar={photo.userphoto}
+              />
+            )}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
